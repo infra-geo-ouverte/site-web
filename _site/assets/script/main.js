efcdfdc38d48b4f350e8d4258a4a4315e5c1c5d1
kboisvert <kboisvert3@gmail.com>
2015-04-23 16:00:19 -0400

    correction js menu telephone

--- _site/assets/script/main.js
+++ _site/assets/script/main.js
@@ -31,9 +31,11 @@ $(document).ready (function(){
 	headerBouton.click(function(){
 		if(headerBouton.hasClass("collapse")){
 			icone.css("top","200px");
+			wrapper.css("top","200px");
 		}
 		else{
 			icone.css("top","500px");
+			wrapper.css("top","500px");
 		}
 		
 	});
